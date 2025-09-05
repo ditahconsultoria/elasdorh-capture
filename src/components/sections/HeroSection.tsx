73d706260347066e48cbafce3fe00492fcd00b8a
Lucas Hora <lucashorarj@gmail.com>
2025-09-04 21:58:08 -0300

    fix: Corrigo diversos erros de responsividade. SE ATENTAR

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { MoveRight } from "lucide-react";
+
+export const HeroSection = () => {
+  return (
+    <section className="relative flex items-center h-[800px] justify-center text-center text-white">
+      <div className="max-w-[1256px] mx-auto px-6">
+        <Image
+          src="/hero-banner.svg"
+          alt="Pessoas reunidas"
+          fill
+          className="object-cover"
+        />
+        {/* Overlay escuro */}
+        <div className="absolute inset-0 bg-black/50" />
+
+        <div className="relative z-10">
+          <h1 className="text-[3.5rem] font-semibold leading-none px-24">
+            Onde mulheres incríveis se encontram para{" "}
+            <span className="text-[#FFB8CA]">transformar o RH</span>
+          </h1>
+          <p className="text-[#E7E0E2] mt-4 text-2xl">
+            Vamos dominar o mundo e precisamos de você conosco!
+          </p>
+          <div className="mt-6 flex gap-4 justify-center">
+            <Button
+              className="bg-[#C40D3A] hover:bg-[#C40D3A] text-white rounded-full font-medium"
+              asChild
+            >
+              <Link href="#">Fazer parte agora</Link>
+            </Button>
+            <Button
+              className="text-white hover:text-[#C40D3A] rounded-full font-medium border hover:border-[#C40D3A] border-white hover:bg-transparent"
+              variant="ghost"
+              asChild
+            >
+              <Link href="#">
+                Sobre nós <MoveRight />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
