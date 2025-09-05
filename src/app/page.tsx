1d54807dfac0e8adaa1d87a8b44a02b084a707b7
Lucas Hora <lucashorarj@gmail.com>
2025-09-04 21:40:17 -0300

    fix: Resolvido problema de versionamento

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { Header } from "./components/header";
+import { IntroSection } from "@/components/sections/IntroSection";
+import { Header } from "../components/layout/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <IntroSection />
     </>
   );
 }
