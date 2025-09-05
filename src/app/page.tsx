3c3518bc36ce226f6cd75f96ff021a7cfe0809fc
Jack <ferreira.marlon@live.com>
2025-09-05 10:05:10 -0300

    feat: initial add about-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "@/components/sections/HeroSection";
 import { IntroSection } from "@/components/sections/IntroSection";
 import { Header } from "@/components/layout/header";
+import { AboutSection } from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <HeroSection />
       <IntroSection />
+      <AboutSection />
     </>
   );
 }
